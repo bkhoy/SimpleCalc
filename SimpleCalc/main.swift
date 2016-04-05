450f294f28b9d7f9c82df05d85949cb6e724fc33
brittney hoy <bkhoy@uw.edu>
2016-04-05 10:52:36 -0700

    Finished

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -11,45 +11,45 @@ import Foundation
 
 print("Enter an expression separated by returns:")
 
-/*
-let num1 = UInt.init(readLine(stripNewline: true)!)!
-let operation = readLine(stripNewline: true)!
-let num2 = UInt.init(readLine(stripNewline: true)!)!
-
-switch operation {
-    case "+": print ("Result: \(num1 + num2) ")
-    case "-": print ("Result: \(num1 - num2) ")
-    case "*": print ("Result: \(num1 * num2) ")
-    case "/": print ("Result: \(num1 / num2) ")
-    case "%": print ("Result: \(num1 % num2) ")
-    default: print ("Try another operation!")
-}
-*/
-
-//count -- take any number of inputs
-//avg --take any number of inputs
-//fact -- can only accept one number
-
 var input = readLine(stripNewline: true)!
 var multiOperand = input.componentsSeparatedByString(" ")
-var trueSize = multiOperand.count - 1
 
-let lastValue = multiOperand[trueSize].lowercaseString
-if lastValue == "count" {
-    print(trueSize)
-}
+if (multiOperand.count == 1) {
 
-else if lastValue == "avg" {
-    var total = 0
-    for value in 1...trueSize {
-        total += value
+    let num1 = UInt.init(readLine(stripNewline: true)!)!
+    let operation = readLine(stripNewline: true)!
+    let num2 = UInt.init(readLine(stripNewline: true)!)!
+
+    switch operation {
+        case "+": print ("Result: \(num1 + num2) ")
+        case "-": print ("Result: \(num1 - num2) ")
+        case "*": print ("Result: \(num1 * num2) ")
+        case "/": print ("Result: \(num1 / num2) ")
+        case "%": print ("Result: \(num1 % num2) ")
+        default: print ("Try another operation!")
     }
-    print(total / trueSize)
-}
-
-else if lastValue == "fact" {
-    var count = multiOperand[0]
     
 } else {
-    print("Check your options and spelling!")
+
+    var trueSize = multiOperand.count - 1
+    let lastValue = multiOperand[trueSize].lowercaseString
+
+    switch lastValue {
+        case "count": print("Total Count: \(trueSize)")
+        case "avg":
+            var total = 0
+            for value in 1...trueSize {
+                total += value
+            }
+            print("Average: \(total / trueSize)")
+        case "fact":
+            var number = Int(multiOperand[0])!
+            var answer = number
+            while number > 1 {
+                number -= 1
+                answer *= number
+            }
+            print("Result: \(answer)")
+        default: print("Check your options and spelling!")
+    }
 }
