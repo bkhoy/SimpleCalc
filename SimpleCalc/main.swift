fba8190306a622636ae9fca0b8afb675e5245bd4
brittney hoy <bkhoy@uw.edu>
2016-04-05 10:36:15 -0700

    Finished second part of homework

--- SimpleCalc/main.swift
+++ SimpleCalc/main.swift
@@ -8,8 +8,10 @@
 
 import Foundation
 
+
 print("Enter an expression separated by returns:")
 
+/*
 let num1 = UInt.init(readLine(stripNewline: true)!)!
 let operation = readLine(stripNewline: true)!
 let num2 = UInt.init(readLine(stripNewline: true)!)!
@@ -22,11 +24,32 @@ switch operation {
     case "%": print ("Result: \(num1 % num2) ")
     default: print ("Try another operation!")
 }
+*/
 
 //count -- take any number of inputs
 //avg --take any number of inputs
 //fact -- can only accept one number
 
+var input = readLine(stripNewline: true)!
+var multiOperand = input.componentsSeparatedByString(" ")
+var trueSize = multiOperand.count - 1
 
-//No matter what, first input will be an integer
-var inputValues = []
+let lastValue = multiOperand[trueSize].lowercaseString
+if lastValue == "count" {
+    print(trueSize)
+}
+
+else if lastValue == "avg" {
+    var total = 0
+    for value in 1...trueSize {
+        total += value
+    }
+    print(total / trueSize)
+}
+
+else if lastValue == "fact" {
+    var count = multiOperand[0]
+    
+} else {
+    print("Check your options and spelling!")
+}
